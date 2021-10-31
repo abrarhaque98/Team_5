ac4c4dc5175a94585b47cacf1c7e9b52187391f7
GilianThompson <85901073+GilianThompson@users.noreply.github.com>
2021-10-31 18:05:22 -0400

    created all initial tables and first join of nya and population table

--- queries.sql
+++ queries.sql
@@ -1,25 +1,64 @@
--- create indexInfo table 
-CREATE TABLE indexInfo(
-	region VARCHAR,
-	exchange VARCHAR,
-	index VARCHAR,
-	currency VARCHAR,
-	PRIMARY KEY (index)
+--create tables 
+CREATE TABLE realGDP(
+	observation_date VARCHAR,
+	quarter VARCHAR,
+	gdpc1 FLOAT,
+	_c3 VARCHAR
 );
-
--- create indexProcessed table 
--- need to change Data column data type to DATE 
-CREATE TABLE indexProcessed (
+-------------------------------------------
+CREATE TABLE inflation(
+	location VARCHAR,
+	indicator VARCHAR, 
+	subject VARCHAR, 
+	measure VARCHAR,
+	frequency VARCHAR,
+	time VARCHAR, 
+	value FLOAT, 
+	flag_codes VARCHAR
+)
+-------------------------------------------
+CREATE TABLE NYA(
 	index VARCHAR, 
-	date VARCHAR, 
-	open FLOAT,
-	high FLOAT,
-	low FLOAT,
-	close FLOAT,
-	adj_close FLOAT,
-	volume FLOAT,
+	date VARCHAR,
+	month VARCHAR, 
+	quarter VARCHAR,
+	open FLOAT, 
+	high FLOAT, 
+	low FLOAT, 
+	close FLOAT, 
+	adj_close FLOAT, 
+	volume FLOAT, 
 	closeUSD FLOAT,
-	FOREIGN KEY(index) REFERENCES indexInfo(index)
+	_c11 VARCHAR,
+	FOREIGN KEY(month) REFERENCES population(month)
+);
+-------------------------------------------
+CREATE TABLE population(
+	observation_date VARCHAR, 
+	month VARCHAR, 
+	LFWA64TTUSM647S FLOAT,
+	PRIMARY KEY(month)
 );
 
 
+-------------------------------------------
+--left join nya and population
+SELECT nya.index, 
+	nya.date,
+	nya.month, 
+	nya.quarter,
+	nya.open,
+	nya.high,
+	nya.low,
+	nya.close,
+	nya.adj_close,
+	nya.volume,
+	nya.closeUSD, 
+	population.observation_date,
+	population.LFWA64TTUSM647S
+INTO first_join
+FROM nya 
+LEFT JOIN population
+ON population.month = nya.month;
+
+SELECT * FROM first_join;
