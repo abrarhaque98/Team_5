3fe3800ecbd06cdfdc5c4e6f910f623e0fba106b
GilianThompson <85901073+GilianThompson@users.noreply.github.com>
2021-10-31 22:57:18 -0400

    cleaned up the join

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-
+--create necessary inital tables 
 CREATE TABLE realGDP(
 	observation_date VARCHAR,
 	quarter VARCHAR,
@@ -6,7 +6,7 @@ CREATE TABLE realGDP(
 	_c3 VARCHAR,
 	PRIMARY KEY(quarter)
 );
--------------------------------------------
+
 CREATE TABLE inflation(
 	location VARCHAR,
 	indicator VARCHAR, 
@@ -18,7 +18,7 @@ CREATE TABLE inflation(
 	flag_codes VARCHAR,
 	PRIMARY KEY(time)
 )
--------------------------------------------
+
 CREATE TABLE NYA(
 	index VARCHAR, 
 	date VARCHAR,
@@ -34,41 +34,40 @@ CREATE TABLE NYA(
 	_c11 VARCHAR,
 	FOREIGN KEY(month) REFERENCES population(month)
 );
--------------------------------------------
+
 CREATE TABLE population(
 	observation_date VARCHAR, 
 	month VARCHAR, 
 	LFWA64TTUSM647S FLOAT,
 	PRIMARY KEY(month)
 );
+
 -------------------------------------------
 
 --number of rows in table 
 SELECT COUNT(*) FROM nya; 
-SELECT COUNT(*) FROM first_join;
+SELECT COUNT(*) FROM complete_join;
+SELECT COUNT(*) FROM inflation;
+SELECT COUNT(*) FROM realGDP;
+SELECT COUNT(*) FROM population;
 
 --drop tables 
 DROP TABLE nya;
 DROP TABLE population;
 DROP TABLE realGDP;
-DROP TABLE first_join;
+DROP TABLE complete_join;
 DROP TABLE inflation;
 
-
---casting observation_date column from string to date data type
+--select tables 
+SELECT * FROM complete_join;
+SELECT * FROM inflation;
 SELECT * FROM realGDP;
-SELECT observation_date, CAST(observation_date as DATE) as Date FROM realGDP;
-
---update realGDP
-UPDATE realGDP
-SET observation_date = CAST(observation_date as DATE)
-
-
+SELECT * FROM nya;
 
 --joining 
 SELECT nya.index, 
 	nya.date,
-	nya.month, 
+	nya.month,
 	nya.quarter,
 	nya.open,
 	nya.high,
@@ -86,7 +85,7 @@ SELECT nya.index,
 	inflation.measure,
 	inflation.frequency,
 	inflation.value
-INTO first_join
+INTO complete_join
 FROM nya 
 LEFT JOIN population
 ON population.month = nya.month
@@ -95,10 +94,15 @@ ON realgdp.quarter = nya.quarter
 LEFT JOIN inflation
 ON inflation.time = nya.month;
 
-SELECT * FROM first_join;
-SELECT * FROM inflation;
-SELECT * FROM realGDP;
-SELECT * FROM nya;
 
+
+--casting observation_date column from string to date data type?
+								--not working-- 	
+SELECT * FROM realGDP;
+SELECT observation_date, CAST(observation_date as DATE) as Date FROM realGDP;
+
+--update realGDP
+UPDATE realGDP
+SET observation_date = CAST(observation_date as DATE)
 
 
