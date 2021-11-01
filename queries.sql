1141f3bb12b27f751690bda7aaf1c9191632065b
GilianThompson <85901073+GilianThompson@users.noreply.github.com>
2021-10-31 22:36:49 -0400

    attempted to join all tables

--- queries.sql
+++ queries.sql
@@ -1,9 +1,10 @@
---create tables 
+
 CREATE TABLE realGDP(
 	observation_date VARCHAR,
 	quarter VARCHAR,
 	gdpc1 FLOAT,
-	_c3 VARCHAR
+	_c3 VARCHAR,
+	PRIMARY KEY(quarter)
 );
 -------------------------------------------
 CREATE TABLE inflation(
@@ -14,7 +15,8 @@ CREATE TABLE inflation(
 	frequency VARCHAR,
 	time VARCHAR, 
 	value FLOAT, 
-	flag_codes VARCHAR
+	flag_codes VARCHAR,
+	PRIMARY KEY(time)
 )
 -------------------------------------------
 CREATE TABLE NYA(
@@ -39,10 +41,31 @@ CREATE TABLE population(
 	LFWA64TTUSM647S FLOAT,
 	PRIMARY KEY(month)
 );
-
-
 -------------------------------------------
---left join nya and population
+
+--number of rows in table 
+SELECT COUNT(*) FROM nya; 
+SELECT COUNT(*) FROM first_join;
+
+--drop tables 
+DROP TABLE nya;
+DROP TABLE population;
+DROP TABLE realGDP;
+DROP TABLE first_join;
+DROP TABLE inflation;
+
+
+--casting observation_date column from string to date data type
+SELECT * FROM realGDP;
+SELECT observation_date, CAST(observation_date as DATE) as Date FROM realGDP;
+
+--update realGDP
+UPDATE realGDP
+SET observation_date = CAST(observation_date as DATE)
+
+
+
+--joining 
 SELECT nya.index, 
 	nya.date,
 	nya.month, 
@@ -55,10 +78,27 @@ SELECT nya.index,
 	nya.volume,
 	nya.closeUSD, 
 	population.observation_date,
-	population.LFWA64TTUSM647S
+	population.LFWA64TTUSM647S,
+	realgdp.gdpc1,
+	inflation.location,
+	inflation.indicator,
+	inflation.subject,
+	inflation.measure,
+	inflation.frequency,
+	inflation.value
 INTO first_join
 FROM nya 
 LEFT JOIN population
-ON population.month = nya.month;
+ON population.month = nya.month
+LEFT JOIN realgdp
+ON realgdp.quarter = nya.quarter
+LEFT JOIN inflation
+ON inflation.time = nya.month;
 
 SELECT * FROM first_join;
+SELECT * FROM inflation;
+SELECT * FROM realGDP;
+SELECT * FROM nya;
+
+
+
